1df66ab8d92f07445db1a220ce8965e8fb0e1d16
prosdev0107 <prosdev0107@outlook.com>
2021-01-28 06:02:40 -0500

    Add api and table helper

---
+++ src/helper/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const getReview = async () => {
+  try {
+    const response = await axios.get(
+      'https://www.fakerestapi.com/datasets/api/v1/iphone-11-amazon-reviews.json'
+    );
+    return response.data.data;
+  } catch (e) {
+    console.log(
+      `There is an error during the API call. Error: ${JSON.stringify(e)}`
+    );
+  }
+};
